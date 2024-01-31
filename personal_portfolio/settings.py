8a3be611213239eca58b6e859dd86629819b091c
Tejas Naik <RNTejas2005@gmail.com>
2024-01-31 12:33:14 +0530

    Django Admin and Images (creating superusers and showing images)

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for personal_portfolio project.
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
